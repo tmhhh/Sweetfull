e9c583f601860f81baaf83e9e6991443d80d6ced
tmhhhh <tmhoang126@gmail.com>
2024-01-27 22:39:25 +0700

    Update icon size

--- app/components/Footer.js
+++ app/components/Footer.js
@@ -6,31 +6,29 @@ import styles from "../page.module.css";
 
 function Footer() {
   return (
-    <>
-      <Flex className={styles.footer} justify="center">
-        <Link
-          className={styles.socialInfo}
-          href="https://www.facebook.com/sweetfull23"
-          target="_blank"
-        >
-          <Image width={47} height={47} src={"/fb-icon.png"} alt="fb" />
-        </Link>
-        <Link
-          className={styles.socialInfo}
-          href="https://www.instagram.com/_sweetfull_/"
-          target="_blank"
-        >
-          <Image width={55} height={55} src={"/insta-icon.png"} alt="insta" />
-        </Link>
-        <Link
-          className={styles.socialInfo}
-          href="https://www.tiktok.com/@_sweetfull"
-          target="_blank"
-        >
-          <Image width={55} height={55} src={"/tiktok-icon.png"} alt="tiktok" />
-        </Link>
-      </Flex>
-    </>
+    <Flex className={styles.footer} justify="center">
+      <Link
+        className={styles.socialInfo}
+        href="https://www.facebook.com/sweetfull23"
+        target="_blank"
+      >
+        <Image width={47} height={47} src={"/fb-icon.png"} alt="fb" />
+      </Link>
+      <Link
+        className={styles.socialInfo}
+        href="https://www.instagram.com/_sweetfull_/"
+        target="_blank"
+      >
+        <Image width={55} height={53} src={"/insta-icon.png"} alt="insta" />
+      </Link>
+      <Link
+        className={styles.socialInfo}
+        href="https://www.tiktok.com/@_sweetfull"
+        target="_blank"
+      >
+        <Image width={55} height={64} src={"/tiktok-icon.png"} alt="tiktok" />
+      </Link>
+    </Flex>
   );
 }
 
